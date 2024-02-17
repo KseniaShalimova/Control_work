f3e68bb46280ec0510f803135ce66f25f93b2a91
Ksenia Shalimova <shalimovakss@xmail.ru>
2024-02-17 19:28:43 +0500

    Написали код, добавили Program.cs

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿string[] StartArray = { "Hello", "2", ":-)" };
+int u = CalculateNumberOfElementsResultArray(StartArray);
+string[] ResultArray = CreateResultArray(StartArray, u);
+OutputArray(ResultArray, u);
+
+int CalculateNumberOfElementsResultArray(string[] StartArray)
+{
+    int u = 0;
+    foreach (string e in StartArray)
+    {
+        if (e.Length <= 3)
+        {
+            u += 1;
+        }
+    }
+    return u;
+}
+
+string[] CreateResultArray(string[] StartArray, int u)
+{
+    string[] ResultArray = new string[u];
+    int i = 0;
+    foreach (string e in StartArray)
+    {
+        if (e.Length <= 3)
+        {
+            ResultArray[i] = e;
+            i += 1;
+        }
+    }
+    return ResultArray;
+}
+
+void OutputArray(string[] ResultArray, int u)
+{
+    Console.Write("[");
+    foreach (string e in ResultArray)
+    {
+        if (e != ResultArray[u - 1])
+        {
+            Console.Write($"{e}, ");
+        }
+        else
+        {
+            Console.Write(e);
+        }
+    }
+    Console.Write("]");
+}
